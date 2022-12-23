d0eef1b2616e8951567c707a2be6685cc95c080c
dikos1705 <dikos1705@gmail.com>
2022-12-23 20:01:58 +0600

    changed version

--- posts/views.py
+++ posts/views.py
@@ -83,3 +83,4 @@ class PostsCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form): 
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
